bbdbec8f42dbeb8126296f0d1f60ab0a0acf0f9e
arttuhar <arttuhar@gmail.com>
2022-02-02 15:36:11 +0200

    FEAT: Add component skeletons for main components, clean files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TrainerComponent } from './trainer/trainer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    TrainerComponent,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule
